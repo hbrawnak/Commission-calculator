46b3966a47cb17165824ea551b5b4bc89ad5668f
Habibur Rahman <hbrawnak@gmail.com>
2021-09-18 09:34:47 +0600

    Doc updated

--- src/Service/API/APIClient.php
+++ src/Service/API/APIClient.php
@@ -4,9 +4,17 @@
 namespace App\CommissionTask\Service\API;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
 class APIClient implements ClientInterface
 {
+    /**
+     * @param $method
+     * @param $url
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
     public function request($method, $url)
     {
         $client = new Client();
